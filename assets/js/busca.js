a0cc646b9cab4eda7a69d0d8ae413b108015ebe3
Rodolfo C. Sant' ana <60012988+rdlf16@users.noreply.github.com>
2021-08-19 23:27:34 -0300

    Inicial login page
    missing footer

---
+++ assets/js/busca.js
@@ -0,0 +1,36 @@
+// document.querySelector('body').addEventListener('load', verdadeiro())
+
+let busca = document.querySelector('.busca__icon')
+busca.addEventListener('click', function(event) {
+    let form = document.querySelector('.busca__form')
+    let expanded = form.getAttribute('aria-expanded')
+    let teste = true
+    expanded == 'true' ? verdadeiro() : falso()
+})
+
+function verdadeiro() {
+    const form = document.querySelector('.busca__form')
+    form.classList.add('busca__form--fechado')
+    form.setAttribute('aria-expanded', 'false')
+}
+
+function falso() {
+    const form = document.querySelector('.busca__form')
+    form.classList.remove('busca__form--fechado')
+    form.setAttribute('aria-expanded', 'true')
+}
+
+
+let p = true;
+elemento.addEventListener('click', toggle(p))
+function toggle(p) {
+    p ? verdadeiro() : falso()
+}
+function verdadeiro() {
+    p = false
+    return true
+}
+function falso() {
+     p = true;
+     return false
+}
